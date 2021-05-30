90c93ca2beb3f92c66c0704a5a4d264ff2e43007
dsusviela <d.susviela@gmail.com>
2021-05-30 19:43:45 -0300

    Add theme colors to app

--- lib/window.py
+++ lib/window.py
@@ -3,13 +3,19 @@ import datetime
 
 class Window:
   def __init__(self):
-    drivers = []
-    times = []
-    for i in range(0, 22):
-      drivers += [f"driver{i}"]
-      times += ['00:00:00.000']
-
-    self.layout = self.__create_initial_layout(drivers, times)
+    my_new_theme = {'BACKGROUND': '#15151e',
+                'TEXT': '#f7f4f1',
+                'INPUT': '#fff8c5',
+                'TEXT_INPUT': '#fff8c5',
+                'SCROLL': '#38383f',
+                'BUTTON': ('#ee0000', '#ee0000'),
+                'PROGRESS': ('#01826B', '#D0D0D0'),
+                'BORDER': 1,
+                'SLIDER_DEPTH': 0,
+                'PROGRESS_DEPTH': 0}
+    sg.theme_add_new('MyNewTheme', my_new_theme)
+    sg.theme('My New Theme')
+    self.layout = self.__create_initial_layout()
     self.main_window = sg.Window(title="F1 TELEMETRY APP - Cucu productions", layout=[self.layout], font="Helvetica 18")
 
   def render(self):
@@ -28,7 +34,13 @@ class Window:
   def __parse_time(self, time):
     return str(datetime.timedelta(seconds=time))[:-3]
 
-  def __create_initial_layout(self, drivers, times):
+  def __create_initial_layout(self):
+    drivers = []
+    times = []
+    for i in range(0, 22):
+      drivers += [f"driver{i}"]
+      times += ['00:00:00.000']
+
     driver_names_layout = []
     for index, driver in enumerate(drivers):
       driver_text = sg.Text(driver, auto_size_text=False, key=f"-DRIVER-POS#{index + 1}-")
